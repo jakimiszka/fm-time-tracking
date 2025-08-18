8bc04330466acd721d54d8f2baf13eddea8b20f1
jakimiszka <jarek.jakimicha@gmail.com>
2025-08-19 00:16:36 +0200

    styling buttons

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const daily_btn = document.querySelector(".daily");
+const weekly_btn = document.querySelector(".weekly");
+const monthly_btn = document.querySelector(".monthly");
+
+daily_btn.addEventListener("click", () => {
+    // daily_btn.classList.add("active");
+    // weekly_btn.classList.remove("active");
+    // monthly_btn.classList.remove("active");
+    console.log("daily");
+});
+
+weekly_btn.addEventListener("click", () => {
+    // daily_btn.classList.add("active");
+    // weekly_btn.classList.remove("active");
+    // monthly_btn.classList.remove("active");
+    console.log("weekly");
+});
+
+monthly_btn.addEventListener("click", () => {
+    // daily_btn.classList.add("active");
+    // weekly_btn.classList.remove("active");
+    // monthly_btn.classList.remove("active");
+    console.log("monthly");
+});
