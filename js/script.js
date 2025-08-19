7a6e84b3494433ae90576375aa2882e886b775d7
jakimiszka <jarek.jakimicha@gmail.com>
2025-08-19 12:14:31 +0200

    card ui fixed (content overlapping image)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 const buttons = document.querySelectorAll(".daily, .weekly, .monthly");
+const dataFields = document.querySelectorAll(".grid__main--card__content--data");
+
+dataFields.forEach(element => {
+    console.log(element.children[0].textContent)
+    console.log(element.children[1].textContent)
+})
 
 new Promise((resolve, reject) => {
 fetch('./data.json')
     .then(respond => {
         resolve(respond.json().then(data => {
             const data2 = data;
-            
+            // process data per time schedule - daily, weekly, monthly
+
             buttons.forEach(btn => {
                 btn.addEventListener("click", () => {
                     buttons.forEach(b => {
