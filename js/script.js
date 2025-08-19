40650416878777e1b5df34b53c5a45169d605af5
jakimiszka <jarek.jakimicha@gmail.com>
2025-08-19 11:48:10 +0200

    data loaded

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 const buttons = document.querySelectorAll(".daily, .weekly, .monthly");
 
-buttons.forEach(btn => {
-    btn.addEventListener("click", () => {
-        buttons.forEach(b => {
-            b.classList.remove("active");
-            b.classList.add("disabled");
-        });
-        btn.classList.add("active");
-        btn.classList.remove("disabled");
+new Promise((resolve, reject) => {
+fetch('./data.json')
+    .then(respond => {
+        resolve(respond.json().then(data => {
+            const data2 = data;
+            
+            buttons.forEach(btn => {
+                btn.addEventListener("click", () => {
+                    buttons.forEach(b => {
+                        b.classList.remove("active");
+                        b.classList.add("disabled");
+                    });
+                    btn.classList.add("active");
+                    btn.classList.remove("disabled");
 
-        if (btn.classList.contains("daily")) {
-            console.log("daily");
-        } else if (btn.classList.contains("weekly")) {
-            console.log("weekly");
-        } else if (btn.classList.contains("monthly")) {
-            console.log("monthly");
-        }
-    });
+                    if (btn.classList.contains("daily")) {
+                        console.log(data2);
+                        console.log("daily");
+                    } else if (btn.classList.contains("weekly")) {
+                        console.log("weekly");
+                    } else if (btn.classList.contains("monthly")) {
+                        console.log("monthly");
+                    }
+                });
+            });
+        }))
+    }).catch(err => {
+        reject(err)
+    })
 });
